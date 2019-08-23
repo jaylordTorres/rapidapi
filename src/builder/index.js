99d4f63d93203ddeb48c1b9528f121d8ae27ab0e
Jay Lord P. Torres <jaylord@umpisa.co>
2019-08-24 01:07:42 +0800

    builder index

--- src/builder/index.js
+++ src/builder/index.js
@@ -1,4 +1,7 @@
 import { getBuilder } from './get.builder'
+import { patchBuilder } from './patch.builder'
 import { postBuilder } from './post.builder'
+import { putBuilder } from './put.builder'
+import { schemaBuilder } from './schema.builder'
 
-export { getBuilder, postBuilder }
+export { getBuilder, patchBuilder, postBuilder, putBuilder, schemaBuilder }
