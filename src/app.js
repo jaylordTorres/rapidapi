fe3e96cdd2db3bde5610959716da1780542396d2
Jay Lord P. Torres <jaylord@umpisa.co>
2019-08-24 11:50:53 +0800

    cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-// app.js
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 import express from 'express'
 import logger from 'morgan'
 
@@ -7,6 +7,8 @@ const app = express()
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+
 app.use(cookieParser())
 
 export default app
