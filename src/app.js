5d318964c4d1a368bd9ec9b683cbb4adce71a6d8
Jay Lord P. Torres <jaylord@umpisa.co>
2019-08-24 11:52:33 +0800

    helmet

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import express from 'express'
+import helmet from 'helmet'
 import logger from 'morgan'
 
 const app = express()
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(helmet());
 app.use(cors())
 
 app.use(cookieParser())
